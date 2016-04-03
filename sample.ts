2ac1df571b8607287bdeb2ea055caddf5639dd78
Patrik Forsberg <patfor@gmail.com>
2016-04-03 22:19:28 +0200

    To XML functionality

--- sample.ts
+++ sample.ts
@@ -1,186 +1,11 @@
-/**
- * Zynaptic node sample
- */
 
-import { ZynapticNode } from "./zynaptic-node";
 var term = require("../zynaptic.turboterminal/turbo-terminal");
 
-/**
- * 	Sample 1: Creating a structured node tree using the
- *	Zynaptic Node implementation
- */
-
-interface IZynArray {
-	[position: number]: ZynNode;
-	length: number;
-	push(item: ZynNode): number;
-}
-
-interface IAttributeList {
-	length: number;
-    [key: string]: any;
-};
-
-class ZynNode {
-	public parentNode: ZynNode = null;
-	public nodeName: string = "";
-	public childNodes: IZynArray = new Array<ZynNode>();
-	private attributes: IAttributeList = [{}];
-		
-	constructor(name: string, parent?: ZynNode) {
-		this.nodeName = name;		
-		this.parentNode = parent;
-	}
-	
-	public addChildNode(name: string): ZynNode {
-		var childNode = new ZynNode(name, this);
-		this.childNodes.push(childNode);
-		
-		return childNode;
-	}
-	
-	public numberOfChildNodes(): number {
-		return this.childNodes.length;
-	}
-	
-	/**
-	 * 
-	 */
-	public getFirstChild(): ZynNode {
-		var childNode: ZynNode = null;
-		
-		if (this.childNodes.length > 0) {
-			childNode = this.childNodes[0];
-		}
-		
-		return childNode;
-	}
-	
-	/**
-	 * 
-	 */
-	public getFirstChildNodeName(): string {
-		var firstChildNodeName = "";
-		var firstChildNode = this.getFirstChild();
-		
-		if (firstChildNode != null) {
-			firstChildNodeName = firstChildNode.nodeName;
-		}
-		
-		return firstChildNodeName;
-	}
-	
-	/**
-	 * 
-	 */
-	public getLastChild(): ZynNode {
-		var childNode: ZynNode = null;
-		var childNodes = this.childNodes;
-		
-		if (childNodes.length > 0) {
-			childNode = childNodes[childNodes.length-1];
-		}
-		
-		return childNode;		
-	}
-	
-	/**
-	 * 
-	 */
-	public isLastChild() {
-		var lastChild = false;
-		
-		if (this.parentNode != null) {
-			var nodeIndex = this.parentNode.getChildNodeIndex(this);
-			if (nodeIndex == this.parentNode.numberOfChildNodes()-1) {
-				lastChild = true;	
-			}
-		}
-		
-		return lastChild;
-	}	
-	
-	/**
-	 * 
-	 */
-	public haveChildNodes(): boolean {
-		return this.getFirstChild != null;
-	}
-	
-	/**
-	 * 
-	 */
-	public getChildNodeIndex(node: ZynNode): number {
-		var nodeIndex: number = -1;
-
-		for (var i = 0; i < this.childNodes.length; i++) {
-			var childNode = this.childNodes[i];
-			if (childNode === node) {
-				nodeIndex = i;
-				break;	
-			}
-		}
-
-		return nodeIndex;
-	}	
-	
-	public getNextSibling(): ZynNode {
-		var node: ZynNode = null;
-		
-		if (this.parentNode != null) {
-			var nodeIndex = this.parentNode.getChildNodeIndex(this);
-			
-		}
-		
-		/*
-	if (Parent<>nil) then
-		begin
-			myIdx := Parent.ChildNodes.IndexOf(Self);
-			if (myIdx>-1) and (myIdx+1<=Parent.ChildNodes.Count-1) then
-				Result := Parent.ChildNodes[myIdx+1];
-
-		end; // end if
-		*/
-		
-		return node;
-	}
-	
-	/**
-	 * Return ZynNode to enable chaining when putting
-	 * attributes...
-	 */
-	public putAttribute(key: string, value: any): ZynNode {
-		this.attributes[key] = value;
-		return this;
-	}
-	
-	public hasAttributeName(name: string): boolean {
-		return (this.childNodes[name] != undefined);
-	}
-
-	public getNodesWithAttribute(attributeName: string): IZynArray {
-		var nodes: IZynArray = new Array<ZynNode>();
-		
-		return nodes;
-	}
-
-	public getAttribute(key: string): any {
-		return this.attributes[key];
-	}
-}
-
-// -- Sample XML Document -- //
-class ZynXmlNode extends ZynNode {
-	constructor(name: string, parent?: ZynNode) {
-		super(name, parent);
-	}	
-}
+import { ZynNode, ZynXmlNode, IZynArray } from "./zynaptic-node";
 
 class ZynapticNodeSample {
-
-
 	terminal = term.TurboTerminal;
-	rootNode: ZynapticNode;
+	xmlNode: ZynXmlNode;
 	
 	constructor() {
 		this.terminal = new term.TurboTerminal();
@@ -195,8 +20,8 @@ class ZynapticNodeSample {
 	}
 
 	runSample() {
-		var xmlNode = new ZynXmlNode("root");
-		var products = xmlNode.addChildNode("products");
+		this.xmlNode = new ZynXmlNode("root");
+		var products = this.xmlNode.addChildNode("products");
 			products.addChildNode("Zybaptic Turbo Terminal").putAttribute("1/5", 3);
 			var badger = products.addChildNode("Zybaptic Badger").putAttribute("1/5", 0);
 				var todo = badger.addChildNode("TODO");
@@ -207,86 +32,10 @@ class ZynapticNodeSample {
 			products.addChildNode("Zybaptic Node").putAttribute("1/5", 4);
 			
 		
+		console.log(this.xmlNode.getFirstChild().nodeName);
+		
 	}
-
-		
-		/*
-		var rootNode = new ZynNode("root");
-		var cpNode = rootNode.addChildNode("Zeppo");
-		var child1 = rootNode.addChildNode("child1");
-			var child1a = child1.addChildNode("child1a");
-			var child1b = child1.addChildNode("child1b");
-			var child1c = child1.addChildNode("child1c");
-
-		var child3 = rootNode.addChildNode("child3");
-		var child4 = rootNode.addChildNode("child4");
-		var child2 = rootNode.addChildNode("child2");
-
-		child1.putAttribute("platform", "Windows");
-		
-		console.log("childIndex", rootNode.getChildNodeIndex(child2));
-		
-		if (child1c.isLastChild()) {
-			console.log("child1b is LAST");
-		} else {
-			console.log("child1b is NOT LAST");
-		}
-		
-		
-		var firstChild = rootNode.getFirstChild();
-		var lastChild = rootNode.getLastChild();
-		
-		if (firstChild != null) {
-			console.log("<First Child> Node", firstChild.nodeName);
-		} else {
-			console.log("<First Child> No Child Nodes");
-		}
-		
-		///
-		
-		if (lastChild != null) {
-			console.log("<Last Child> Node", lastChild.nodeName);
-		} else {
-			console.log("<Last Child> No Child Nodes");
-		}
-
-		console.log("child3 is last child", child3.isLastChild());
-		console.log("child2 is last child", child2.isLastChild());
-
-		this.echoNodeList(rootNode.childNodes);
-	}
-	*/
 }
 
 var zynNodeSample = new ZynapticNodeSample();
 zynNodeSample.runSample();
-
-
-/*		var terminal = this.terminal;
-		var self = this;
-		this.rootNode = new ZynapticNode();
-		var root = this.rootNode;
-
-		var child = new ZynapticNode("Languages", root);
-		root.childNodes.push()
-
-		var child = this.rootNode.newChildNode("Programming Languages");
-		child.putAttrib("ove", true);
-		
-
-		var children = root.childNodes;
-		for (var i = 0; i < children.length; i++) {
-		}
-
-		//	console.log("Child", children[0]);
-
-		this.rootNode.childNodes.forEach(element => {
-			terminal.echo(element);
-		});
-
-//		this.terminal.echoArray("rootNode.ChildNodes", this.rootNode.ChildNodes);
-		
-		//terminal.echo("Preparing output...");
-
-		//this.terminal.debugOutput(this.rootNode.childNodes);
-	} */
--- zynaptic-node.ts
+++ zynaptic-node.ts
@@ -1,109 +1,202 @@
 /**
- * @name Zynaptic Node
- * @description 
- * @date 2015-03-12
- * @author Patrik Fosberg <mail@patrikforsberg.net>
- * @web www.patrikforsberg.net
- * @copyright Patrik Forsberg, some rights reserved
+ *	@name Zynaptic Node
+ *	@description	
+ *	@date 2015-03-	2
+ *	@author Patrik Forsberg <mail@patrik.guru>
+ *	@web www.patrik.guru
+ *	@copyright Patrik Forsberg, some rights reserved
  * 
- * @history
- * TypeScript implementation of my old work horse
- * PutteNode, written in Object Pascal, the original
- * implementation as well as C#, Java and Erlang
- * implementations can be found here:
- * www.patrik.guru/zynaptic-node/implementations
+ *	@history
+ *	TypeScript implementation of my old work horse
+ *	PutteNode, written in Object Pascal, the original
+ *	implementation as well as C#, Java and Erlang,
+ *	these implementations can be found here:
+ *	www.patrik.guru/zynaptic-node/implementations
  * 
- * In order to use this piece of software, this file header
- * must remain intact.
+ *	In order to use this piece of software, this file header
+ *	must remain intact.
  * 
- * Zynaptic Node is licensed under the Creative Commons
- * ### License, for more information, follow this link:
- * https://creativecommons.org
+ *	Zynaptic Node is licensed under the Creative Commons
+ *	### License, for more information, follow this link:
+ *	https://creativecommons.org
 */
 
-/// <reference path="typings/main.d.ts" />
+var term = require("../zynaptic.turboterminal/turbo-terminal");
 
-
-/** DEEP TRAVERSAL
- 
- 
- var findObjectByLabel = function(obj, label) {
-    if(obj.label === label) { return obj; }
-    for(var i in obj) {
-        if(obj.hasOwnProperty(i)){
-            var foundLabel = findObjectByLabel(obj[i], label);
-            if(foundLabel) { return foundLabel; }
-        }
-    }
-    return null;
-};
- 
+/**
+ * 	Sample 1: Creating a structured node tree using the
+ *	Zynaptic Node implementation
  */
 
-const ROOT_NODE_NAME = "root";
-
-import { KeyValueModel } from "./core/key-value.model"
-
-// External Dependency
-//import { TurboTerminal } from "../zynaptic.turboterminal/turbo-terminal"
-
-interface StructuredNode {
-    ChildNodes: StructuredNode[];
+interface IZynArray {
+	[position: number]: ZynNode;
+	length: number;
+	push(item: ZynNode): number;
 }
 
-class ZynapticNode { // implements StructuredNode {
-	public parentNode: ZynapticNode = null;
-    public nodeName: string; 
-    //ChildNodes: StructuredNode[];
-	public childNodes: ZynapticNode[];
+interface IAttributeList {
+	length: number;
+    [key: string]: any;
+};
 
-
-    constructor(nodeName?: string, parentNode?: ZynapticNode) {
-		if (nodeName === undefined) {
-			this.nodeName = ROOT_NODE_NAME;
+class ZynNode {
+	public parentNode: ZynNode = null;
+	public nodeName: string = "";
+	public nodeValue: string = "";
+	public childNodes: IZynArray = new Array<ZynNode>();
+	private attributes: IAttributeList = [{}];
+		
+	constructor(name: string, parent?: ZynNode) {
+		this.nodeName = name;		
+		this.parentNode = parent;
+	}
+	
+	public addChildNode(name: string): ZynNode {
+		var childNode = new ZynNode(name, this);
+		this.childNodes.push(childNode);
+		
+		return childNode;
+	}
+	
+	public numberOfChildNodes(): number {
+		return this.childNodes.length;
+	}
+	
+	public empty(): boolean {
+		return this.nodeValue.length == 0;
+	}
+	
+	/**
+	 * 
+	 */
+	public getFirstChild(): ZynNode {
+		var childNode: ZynNode = null;
+		
+		if (this.childNodes.length > 0) {
+			childNode = this.childNodes[0];
 		}
 		
-		this.parentNode = parentNode;
-        this.childNodes =  []; 
-    }
-    
-    get NodeName(): string {
-        return this.nodeName;
-    }
-    
-    set NodeName(value: string) {
-        this.nodeName = value;
-    }
-
-    public newChildNode(nodeName?: string): ZynapticNode {
-        var newNode = new ZynapticNode(nodeName);
-		console.log('NewNode', newNode);
-		
-        this.childNodes.push(newNode, this);
+		return childNode;
+	}
 	
-        return newNode;
-    }
-	
-	/********************************************************
+	/**
 	 * 
-	 * 		Attribute Related Functionality
-	 * 
-	 ********************************************************/
-	public findAttribute(name: String): KeyValueModel {
-		var keyValuePair = null;
+	 */
+	public getFirstChildNodeName(): string {
+		var firstChildNodeName = "";
+		var firstChildNode = this.getFirstChild();
 		
-		return keyValuePair;	
-	}
-
-	public putAttrib(name: string, value: any) {
+		if (firstChildNode != null) {
+			firstChildNodeName = firstChildNode.nodeName;
+		}
 		
-	}
-
-	public getAttributeValue(name: string): any {
-		
+		return firstChildNodeName;
 	}
 	
+	/**
+	 * 
+	 */
+	public getLastChild(): ZynNode {
+		var childNode: ZynNode = null;
+		var childNodes = this.childNodes;
+		
+		if (childNodes.length > 0) {
+			childNode = childNodes[childNodes.length-1];
+		}
+		
+		return childNode;		
+	}
+	
+	/**
+	 * 
+	 */
+	public isLastChild() {
+		var lastChild = false;
+		
+		if (this.parentNode != null) {
+			var nodeIndex = this.parentNode.getChildNodeIndex(this);
+			if (nodeIndex == this.parentNode.numberOfChildNodes()-1) {
+				lastChild = true;	
+			}
+		}
+		
+		return lastChild;
+	}	
+	
+	/**
+	 * 
+	 */
+	public haveChildNodes(): boolean {
+		return this.getFirstChild != null;
+	}
+	
+	/**
+	 * 
+	 */
+	public getChildNodeIndex(node: ZynNode): number {
+		var nodeIndex: number = -1;
 
+		for (var i = 0; i < this.childNodes.length; i++) {
+			var childNode = this.childNodes[i];
+			if (childNode === node) {
+				nodeIndex = i;
+				break;	
+			}
+		}
+
+		return nodeIndex;
+	}	
+	
+	public getNextSibling(): ZynNode {
+		var node: ZynNode = null;
+		
+		if (this.parentNode != null) {
+			var nodeIndex = this.parentNode.getChildNodeIndex(this);
+			
+		}
+		
+		/*
+	if (Parent<>nil) then
+		begin
+			myIdx := Parent.ChildNodes.IndexOf(Self);
+			if (myIdx>-1) and (myIdx+1<=Parent.ChildNodes.Count-1) then
+				Result := Parent.ChildNodes[myIdx+1];
+
+		end; // end if
+		*/
+		
+		return node;
+	}
+	
+	/**
+	 * Return ZynNode to enable chaining when putting
+	 * attributes...
+	 */
+	public putAttribute(key: string, value: any): ZynNode {
+		this.attributes[key] = value;
+		return this;
+	}
+	
+	public hasAttributeName(name: string): boolean {
+		return (this.childNodes[name] != undefined);
+	}
+
+	public getNodesWithAttribute(attributeName: string): IZynArray {
+		var nodes: IZynArray = new Array<ZynNode>();
+		
+		return nodes;
+	}
+
+	public getAttribute(key: string): any {
+		return this.attributes[key];
+	}
 }
 
-export { ZynapticNode }
+// -- Sample XML Document -- //
+class ZynXmlNode extends ZynNode {
+	constructor(name: string, parent?: ZynNode) {
+		super(name, parent);
+	}	
+}
+
+export { ZynNode, ZynXmlNode, IZynArray }
